d52d3bd2c72c711329a958b24b63f6c50560dc60
Void700 <mamadsf846@gmail.com>
2026-01-03 19:26:50 +0330

    update

--- main.py
+++ main.py
@@ -6,31 +6,8 @@ from src.request import Request
 from src.header_gen import UA
 from src.Reader import SubReader
 from requests.exceptions import RequestException
-class Req():
-    def __init__(self,domain):
-        self.domain = domain
-    def Headergen(self):
-        call = UA()
-        headers = {
-            "Host": f"{self}",
-            "User-Agent": f"{call.ua()}",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive",
-        }
-        return headers
-
-    def Req(self,domain,header=Headergen(),timeout:int = 10):
-            for i in SubReader():
-                try:
-                    call = Request(domain=domain,subdomain=i)
-                    respone = call.get_req(header=header,Timeout=10)
-                    print(respone)
-                except RequestException as err:
-                    continue
-
+call = Request("linka.ir","api")
+print(call.get_req())
 # console = Console()
 # OS_info = Panel.fit(f"[blue]OS:{Detect()}")
 # console.print(OS_info)
-print(Req("trexmine.com"))
